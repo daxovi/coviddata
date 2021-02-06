adaa8c41f460dc7825594fb99b674115915e64a8
daxovi <48390551+daxovi@users.noreply.github.com>
2021-02-06 16:12:51 +0100

    stacked bar graph

--- testovani.js
+++ testovani.js
@@ -1,7 +1,7 @@
 var testovani = document.getElementById('testovaniChart').getContext('2d');
 var testovaniChart = new Chart(testovani, {
     // The type of chart we want to create
-    type: 'line',
+    type: 'bar',
 
     // The data for our dataset
     data: {
@@ -9,21 +9,15 @@ var testovaniChart = new Chart(testovani, {
         datasets: [
             {
                 label: 'PCR',
-                backgroundColor: 'rgb(255, 99, 132, 0)',
+                backgroundColor: 'rgb(57, 169, 53)',
                 borderColor: 'rgb(57, 169, 53)',
                 data: [316, 1016, 643, 760, 630, 315, 1025, 955, 782, 614, 214, 1027, 862, 799, 661]
             },
             {
                 label: 'antigenní',
-                backgroundColor: 'rgb(255, 99, 132,0)',
+                backgroundColor: 'rgb(255, 150, 0)',
                 borderColor: 'rgb(255, 150, 0)',
                 data: [260, 267, 254, 209, 241, 290, 287, 277, 262, 259, 303, 279, 291, 305, 310]
-            },
-            {
-                label: 'odběry',
-                backgroundColor: 'rgb(255, 99, 132,0)',
-                borderColor: 'rgb(92, 195, 237)',
-                data: [102, 297, 292, 249, 233, 121, 233, 309, 227, 254, 84, 320, 325, 285, 242]
             }
         ]
     },
@@ -31,5 +25,21 @@ var testovaniChart = new Chart(testovani, {
     // Configuration options go here
     options: {
         aspectRatio: 1,
+        legend: {
+            labels: {
+                boxWidth: 5,
+                usePointStyle: true,
+                fontColor: '#000',
+            },
+            // align: 'start',
+        },
+        scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        },
     }
 });
