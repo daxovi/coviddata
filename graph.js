6eea57ca332968ae1606799a2517d8560923e5ab
daxovi <48390551+daxovi@users.noreply.github.com>
2021-02-13 15:32:34 +0100

    weekdays data

--- graph.js
+++ graph.js
@@ -2,17 +2,27 @@ datumy = datumy.reverse();
 dataJip = dataJip.reverse();
 dataStandardni = dataStandardni.reverse();
 dataOckovani = dataOckovani.reverse();
-
-for (let i = 0; i < dataOckovani.length-1; i++) {
-    dataOckovani[i+1] = dataOckovani[i] + dataOckovani[i+1];
-}
-
 dataPCR = dataPCR.reverse();
 dataAntigenni = dataAntigenni.reverse();
 dataOdbery = dataOdbery.reverse();
 
+for (let i = 0; i < dataOckovani.length - 1; i++) {
+    dataOckovani[i + 1] = dataOckovani[i] + dataOckovani[i + 1];
+}
+
+var pocetZobrazeni = 20;
+
+datumy = datumy.slice(datumy.length - pocetZobrazeni, datumy.length);
+dataJip = dataJip.slice(dataJip.length - pocetZobrazeni, dataJip.length);
+dataStandardni = dataStandardni.slice(dataStandardni.length - pocetZobrazeni, dataStandardni.length);
+dataOckovani = dataOckovani.slice(dataOckovani.length - pocetZobrazeni, dataOckovani.length);
+dataPCR = dataPCR.slice(dataPCR.length - pocetZobrazeni, dataPCR.length);
+dataAntigenni = dataAntigenni.slice(dataAntigenni.length - pocetZobrazeni, dataAntigenni.length);
+dataOdbery = dataOdbery.slice(dataOdbery.length - pocetZobrazeni, dataOckovani.length);
+
+
 function vykresliHospitalizovani() {
-    document.getElementById('soucet').innerHTML = dataJip[dataJip.length-1] + dataStandardni[dataStandardni.length-1];
+    document.getElementById('soucet').innerHTML = dataJip[dataJip.length - 1] + dataStandardni[dataStandardni.length - 1];
     var hospitalizovani = document.getElementById('hospitalizovaniChart').getContext('2d');
     var hospitalizovaniChart = new Chart(hospitalizovani, {
         // The type of chart we want to create
@@ -45,11 +55,11 @@ function vykresliHospitalizovani() {
                     boxWidth: 12,
                     //usePointStyle: true,
                     fontColor: '#394A59',
-                    
+
                 },
                 position: 'bottom',
                 align: 'start',
-                
+
             },
             scales: {
                 xAxes: [{
@@ -82,7 +92,7 @@ function vykresliHospitalizovani() {
 }
 
 function vykresliTestovani() {
-    document.getElementById('soucet').innerHTML = dataPCR[dataPCR.length-1] + dataAntigenni[dataAntigenni.length-1];
+    document.getElementById('soucet').innerHTML = dataPCR[dataPCR.length - 1] + dataAntigenni[dataAntigenni.length - 1];
     var testovani = document.getElementById('testovaniChart').getContext('2d');
     var testovaniChart = new Chart(testovani, {
         // The type of chart we want to create
@@ -104,12 +114,12 @@ function vykresliTestovani() {
                     borderColor: '#CACF00',
                     data: dataAntigenni,
                 },
-/*                 {
-                    label: 'odběry',
-                    // backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(92, 195, 237)',
-                    data: dataOdbery,
-                } */
+                /*                 {
+                                    label: 'odběry',
+                                    // backgroundColor: 'rgb(255, 99, 132)',
+                                    borderColor: 'rgb(92, 195, 237)',
+                                    data: dataOdbery,
+                                } */
             ]
         },
 
@@ -157,7 +167,7 @@ function vykresliTestovani() {
 }
 
 function vykresliOckovani() {
-    document.getElementById('soucet').innerHTML = dataOckovani[dataOckovani.length-1];
+    document.getElementById('soucet').innerHTML = dataOckovani[dataOckovani.length - 1];
     var ockovani = document.getElementById('ockovaniChart').getContext('2d');
     var ockovaniChart = new Chart(ockovani, {
         // The type of chart we want to create
