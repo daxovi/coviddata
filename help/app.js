c06391137d8fd8806e3b2d40529ebb77815fb978
daxovi <48390551+daxovi@users.noreply.github.com>
2021-11-30 07:53:21 +0100

    multiline test

--- help/app.js
+++ help/app.js
@@ -2,9 +2,11 @@ const text = document.querySelector("#text");
 const button = document.querySelector("button");
 const result = document.querySelector("#result");
 button.addEventListener("click", (arg) => {
+    console.log(arg)
     arg.preventDefault();
     const input = text.value;
     const inputArr = input.split("\n");
+    console.log(inputArr);
     const inputArrRev = inputArr.reverse();
     console.log(inputArrRev.join(", "));
     result.textContent = inputArrRev.join(", ");
